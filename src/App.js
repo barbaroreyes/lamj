ce51ed1f34b3627670724ed0b0cf10588e264e21
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-17 16:00:05 -0500

    added-materialui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import StripeCheckout from 'react-stripe-checkout';
+// import StripeCheckout from 'react-stripe-checkout';
 import  AppBar from   './compo/AppBar';
+// import List from './compo/ListImages'
 import './App.css';
 
 function App() {
 
-  const onToken = (token) => {
-    console.log(token)
-  }
+  // const onToken = (token) => {
+  //   console.log(token)
+  // }
   return (
     <div className="App">
-    <AppBar/>
+      <AppBar/>
+    
       {/* <StripeCheckout
         token={onToken}
         stripeKey="pk_live_51JkyRMLm3LiulVebwtzLghNlEl99W5u7kXuHgQfFmx8t8tG02z25ezrRktAlx2OeFVMrIYQEzp1P8AQ0v48Gu1B300P3z7YUIz"
