e5fcc8844ae5cbc1faaf57b46b916e629283bf08
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-19 00:50:42 -0500

    update

--- src/compo/Main.js
+++ src/compo/Main.js
@@ -1,17 +1,27 @@
 import * as React from 'react';
-
+import {AppBar,Collapse,IconButton,Toolbar} from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
   root: {
     backgroundColor: 'transparent',
     color: (props) => props.color,
-  },
+  },godawn:{
+      color:'gold',
+      fontSize: '3rem',
+    
+  }
 });
 
 export default function MyComponent(props) {
   const classes = useStyles(props);
   return <main className={classes.root} >
-    <h1 className='name'>La Mango <span>Jewellery</span></h1>
+    <h1 className='name'>
+        La Mango <span>Jewellery</span>
+    </h1>
+    <IconButton className={classes.button}>
+        <ExpandMoreIcon className={classes.godawn}/>
+    </IconButton>
   </main>;
 }
